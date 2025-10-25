bd37b42777f8540217987ad3e78b032974d50bb1
abatatsa99 <abatatsa311@gmail.com>
2025-10-25 22:32:58 +0700

    Update srkandi313.py

--- srkandi313.py
+++ srkandi313.py
@@ -100,7 +100,7 @@ class httpth1(threading.Thread):
                 print(f"\033[35m[] \033[94mSRIKANDI-313  \033[33mSent to  \033[97m••>  \033[41m" +str(url)+ " \033[0m")
                 print(f"\033[38;5;39m[] \033[31mSRIKANDI-313  \033[34mSent to  \033[33m••>  \033[44m" +str(url)+ " \033[0m")
             except requests.exceptions.ConnectionError:
-                print(f"\033[97m[] \033[102mSRIKANDI-313\033[0m  \033[7mConnection-error\033[0m \033[95mServer Maybe down\033[0m")
+                print(f"\033[97m[] \033[103mSRIKANDI-313\033[0m  \033[7mConnection-error\033[0m \033[31mServer Maybe down\033[0m")
 
                 pass
             except requests.exceptions.InvalidSchema:
