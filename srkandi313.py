17b278d4d2094870a917a5500ffd8032aa029b7d
abatatsa99 <abatatsa311@gmail.com>
2025-10-25 11:06:37 +0700

    Update srkandi313.py

--- srkandi313.py
+++ srkandi313.py
@@ -36,8 +36,8 @@ print("""
 \033[31m║\033[36m║█║
 \033[31m║\033[36m║█████╗
 \033[31m║\033[36m╚═══╗█║
-\033[31m║\033[36m   ║█║
-\033[31m║\033[36m█████║    
+\033[31m║\033[36m    ║█║
+\033[31m║\033[36m║█████║    
 \033[31m║\033[36m╚════╝  
 \033[31m║\033[36m
 \033[31m║\033[36m  
@@ -103,9 +103,9 @@ class httpth1(threading.Thread):
                 requests.get(randomized_url, headers=headers)
                 u += 1
                 print(f"\033[35m[] \033[94mSRIKANDI-313  \033[33mSent to  \033[97m••>  \033[32m[" +str(url)+ "]\033[0m")
-                print(f"\033[38;5;39m[] \033[31mSRIKANDI-313  \033[94mSent to  \033[33m••>  \033[38;5;206m[" +str(url)+ "]\033[0m")
+                print(f"\033[38;5;39m[] \033[31mSRIKANDI-313  \033[34mSent to  \033[33m••>  \033[38;5;206m[" +str(url)+ "]\033[0m")
             except requests.exceptions.ConnectionError:
-                print(f"\033[97m[] \033[102mSRIKANDI-313  \033[41mConnection-error...!! \033[0m")
+                print(f"\033[97m[] \033[102mSRIKANDI-313\033[0m  \033[41mConnection-error\033[0m\033[96mWebs maybe down\033[0m")
 
                 pass
             except requests.exceptions.InvalidSchema:
