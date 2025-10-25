1825e92c35a69590c89455318ecd312ac060efd7
abatatsa99 <abatatsa311@gmail.com>
2025-10-25 10:08:57 +0700

    Update srkandi313.py

--- srkandi313.py
+++ srkandi313.py
@@ -103,8 +103,9 @@ class httpth1(threading.Thread):
                 requests.get(randomized_url, headers=headers)
                 u += 1
                 print(f"\033[35m[] \033[96mSRIKANDI-313  \033[31mSent to  \033[95m••>   \033[97m[" +str(url)+ "]\033[0m")
+            
             except requests.exceptions.ConnectionError:
-                print(f"\033[97m[] \033[102mSRIKANDI-313  \033[7mConnection-error...!! "]\033[0m")
+                print(f"\033[97m[] \033[102mSRIKANDI-313  \033[41mConnection-error...!! \033[0m")
 
                 pass
             except requests.exceptions.InvalidSchema:
